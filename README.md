c672c49ac7128e5d22e3b0ae85e76c1d2c343600
Sierra Gregg <sierragregg@Gmail.com>
2016-06-14 14:43:25 -0500

    Added line-through for checked items

--- README.md
+++ README.md
@@ -5,5 +5,8 @@
 2. Ability to remove items 
 3. Ability to check and uncheck items as purchased
 
+## Extras
+1. Add strickthrough to a checked item
+
 ## Wireframe
 [Desktop view](https://wireframe.cc/XJzcFO)
--- js/app.js
+++ js/app.js
@@ -6,9 +6,15 @@ $(document).ready(function() {
         $('ul').append("<li><input type=\"checkbox\" name=\"list-items\" value=\"1\" id=\"one\"><label for=\"one\">" + user_string + "</label><button type=\"button\" id=\"button-one\">Delete</button></li>")
     });
 
-$(document).on("click", "button", function() {
-    console.log("button clicked!");
-    $(this).parent().remove();
-});
+	$(document).on("click", "button", function() {
+	    console.log("button clicked!");
+	    $(this).parent().remove();
+	});
 
+	$(document).on( "change", "input[type='checkbox']",
+    function(){
+        if ($(this).is(':checked')) {
+            $(this).next().css('text-decoration', 'line-through');
+        }
+    });
 });
