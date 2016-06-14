85ac59f20b1645009f44672a9bd1787cfa4b498e
Alex Bang <alexbang@hotmail.com>
2016-06-14 17:11:27 -0400

    Add uncheck all feature

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    var variable = 9;
     $('form').submit(function() {
         var user_string = $('input[type="text"]').val();
         event.preventDefault();
@@ -6,7 +7,7 @@ $(document).ready(function() {
         $('ul').append("<li><input type=\"checkbox\" name=\"list-items\" value=\"1\" id=\"one\"><label for=\"one\">" + user_string + "</label><button type=\"button\" id=\"button-one\">Delete</button></li>")
     });
 
-	$(document).on("click", "button", function() {
+	$(document).on("click", "fieldset button", function() {
 	    console.log("button clicked!");
 	    $(this).parent().remove();
 	});
@@ -15,8 +16,13 @@ $(document).ready(function() {
     function(){
         if ($(this).is(':checked')) {
             $(this).next().css('text-decoration', 'line-through');
-        } else if ( $(this).not(":checked") ){
+        } else if (!$(this).prop("checked")){
         	$(this).next().css('text-decoration', 'none');
         }
+    });
+
+    $(document).on("click", "#uncheck-all", function() {
+        console.log("this works");
+        $("input[type='checkbox']:checked").prop("checked", false).next().css('text-decoration', 'none');
     });
 });
