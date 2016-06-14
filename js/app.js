aacac78d6f20304e7bc01a2ba50b75d3ee5b34e3
Alex Bang <alexbang@hotmail.com>
2016-06-14 15:21:45 -0400

    Complete even handling

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('form').submit(function() {
+        var user_string = $('input[type="text"]').val();
+        event.preventDefault();
+        $('input[type="text"]').val("");
+        $('ul').append("<li><input type=\"checkbox\" name=\"list-items\" value=\"1\" id=\"one\"><label for=\"one\">" + user_string + "</label><button type=\"button\" id=\"button-one\">Delete</button></li>")
+    });
+
+$(document).on("click", "button", function() {
+    console.log("button clicked!");
+    $(this).parent().remove();
+});
+
+});
